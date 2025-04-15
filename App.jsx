54724f70e2285a957bb15f2b34a9edc96f188634
Justamanuelax <justamanuelax@gmail.com>
2025-04-15 15:33:23 +0200

    The PageNav was added to the relevant pages however the reactRouter is going to update to v7 and have a different structure in a couple of months

--- App.jsx
+++ App.jsx
@@ -5,19 +5,15 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound"; // Import the PageNotFound component
 function App() {
   return (
-    <BrowserRouter>
-      <>
-        <h2> World Wise</h2> 
-        {/* This part the World Wise is shown on all pages regardless according to its postioning */}
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="*" element={<PageNotFound/>} /> {/* Added About route */}
-          {/* Define your routes here */}
-        </Routes>
-      </>
-    </BrowserRouter>
+<BrowserRouter>
+<Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="*" element={<PageNotFound/>} /> {/* Added About route */}
+      {/* Define your routes here */}
+    </Routes>
+</BrowserRouter>
   );
 }
 
--- pages/Pricing.jsx
+++ pages/Pricing.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 function Pricing(){
     return (
         <div>
+            <PageNav/>
             <h1>Pricing</h1>
             <p>This is the Pricing page.</p>
+            <p>Here you can find the pricing details for our products.</p>
+            <Link to="/">Go to Homepage</Link>
         </div>
     );
 }
